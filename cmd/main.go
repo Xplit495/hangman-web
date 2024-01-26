7f535ac467b34b675aa2fc1ebc2953abb5c54eb1
carquentin <quentin.carrola@ynov.com>
2024-01-26 14:31:30 +0100

    beggin html

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
+import "hangman-web/internal/server"
 
 func main() {
-	var (
-		difficultyFile string
-	)
-
-	if len(os.Args) > 2 {
-		fmt.Println("Trop d'arguments, fin du programme.")
-		os.Exit(1)
-	} else if len(os.Args) == 2 {
-		difficultyFile = os.Args[1]
-	} else {
-		fmt.Println("Pas de difficulté choisie, fin du programme.")
-		os.Exit(1)
-	}
-
-	if difficultyFile == "easy.txt" {
-		fmt.Println("Difficulté facile")
-	} else if difficultyFile == "medium.txt" {
-		fmt.Println("Difficulté moyenne")
-	} else if difficultyFile == "hard.txt" {
-		fmt.Println("Difficulté difficile")
-	}
+	server.LaunchServer()
 }
---
+++ internal/server/launchServer.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func LaunchServer() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileServer := http.FileServer(http.Dir(wd + "\\web"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			http.ServeFile(w, r, wd+"\\web\\html\\index.html")
+		} else {
+			fileServer.ServeHTTP(w, r)
+		}
+	})
+
+	fmt.Println("Pour accéder au jeu -> http://localhost:8080/")
+	err1 := http.ListenAndServe(":8080", nil)
+	if err1 != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
