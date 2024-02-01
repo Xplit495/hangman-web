7cc6a7bed40fe1eec930a8d1b3cad911f16e72a8
carquentin <quentin.carrola@ynov.com>
2024-02-01 14:23:22 +0100

    Progess

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module hangman-web
 
 go 1.21.6
 
-require github.com/Xplit495/hangman-classic v0.0.0-20240126142940-feedc3275e46
+require github.com/Xplit495/hangman-classic v0.0.0-20240126150839-7248d2876334
--- internal/server/launchServer.go
+++ internal/server/launchServer.go
@@ -17,7 +17,6 @@ func LaunchServer() {
 	}
 
 	fileServer := http.FileServer(http.Dir(wd + "\\web"))
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/" {
 			http.ServeFile(w, r, wd+"\\web\\html\\index.html")
@@ -26,10 +25,17 @@ func LaunchServer() {
 		}
 	})
 
+	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			r.ParseForm()
+			//username := r.FormValue("username")
+			//difficulty := r.FormValue("difficulty")
+		}
+	})
+
 	fmt.Println("Pour accéder au jeu -> http://localhost:8080/")
-	err1 := http.ListenAndServe(":8080", nil)
-	if err1 != nil {
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
-
 }
