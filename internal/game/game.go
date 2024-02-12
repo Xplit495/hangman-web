2a2fda3f89590fd1efc2979f8ae49b45fef87379
Darkfrost <eddy.vieillemaringe@ynov.com>
2024-02-12 15:57:42 +0100

    fix old code

--- internal/game/game.go
+++ internal/game/game.go
@@ -25,14 +25,15 @@ func CheckProposition(liveJose int, choice string, arrSelectWord []string, wordP
 		if letterAlreadyUse || wordAlreadyUse {
 			badWarning = "Attention vous avez déjà essayé ce mot ou cette lettre !"
 		} else {
-			goodWarning = "Correct !"
 			if wordFind {
 				os.Exit(0)
 			}
 			if !wordFind && len(choiceToLowerStrings) > 1 {
 				liveJose -= 2
-			} else {
+			} else if !letterFind {
 				liveJose--
+			} else {
+				goodWarning = "Correct !"
 			}
 		}
 		fmt.Println(liveJose)
